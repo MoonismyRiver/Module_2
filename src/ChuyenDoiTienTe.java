180e03212d71862c8b1c3b61c5d3241882f2bcdf
MoonismyRiver <keepermen1962gmail.com>
2022-05-09 16:47:30 +0700

    baitap 1 Java

---
+++ src/ChuyenDoiTienTe.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ChuyenDoiTienTe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double vnd = 23000;
+        System.out.println("Bạn hãy nhập số USD cần quy đổi : ");
+        double usd ;
+        usd = scanner.nextDouble();
+        double quydoi = usd * vnd;
+        System.out.println("Giá trị sau quy đổi : " + quydoi + "VNĐ");
+    }
+}
---
+++ src/HienThiLoiChao.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("nhập tên bạn : ");
+        String name = scanner.nextLine();
+        System.out.println("Chào bạn " + name);
+    }
+}
