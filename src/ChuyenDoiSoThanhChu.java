2fa1e7d4d866eea2b36f3d0f4e30c80f09c527a2
MoonismyRiver <keepermen1962gmail.com>
2022-05-10 00:43:04 +0700

    sửa đọc số

--- src/ChuyenDoiSoThanhChu.java
+++ src/ChuyenDoiSoThanhChu.java
@@ -1,97 +1,109 @@
 import java.util.Scanner;
 
-public class ChuyenDoiSoThanhChu{
+public class ChuyenDoiSoThanhChu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (1 > 0) {
+
             System.out.print("nhập số cần đọc (0-999): ");
-            String input = sc.nextLine();
-            while (!(input.matches("^[1-9][0-9]?[0-9]$") ||
-                    input.matches("^[0-9]$"))) {
-                System.out.println("không hợp lệ!");
-                System.out.print("nhập số cần đọc (0-999): ");
-                input = sc.nextLine();
-            }
-            String[] inputRay = input.split("");
-            switch (inputRay.length - 1) {
-                case 1:
-                    if (inputRay[1].equals("0")) {
-                        System.out.println("zero");
-                    } else {
-                        System.out.println(tenDown(inputRay[1]));
-                    }
+            int input = sc.nextInt();
+            int hangtram = input / 100;
+            int donvi = input % 10;
+            int hangchuc = ((input - donvi) / 10)%10;
+            String dochangtram=sc.nextLine();
+            String dochangchuc=sc.nextLine();
+            String docdonvi=sc.nextLine();
+            switch (hangtram){
+                case 1 :
+                    dochangtram="một";
                     break;
                 case 2:
-                    System.out.println(tenUp(inputRay[1], tenDown(inputRay[2])));
+                    dochangtram = "hai";
                     break;
-                case 3:
-                    if ((inputRay[2].equals("0")) && (inputRay[3].equals("0"))) {
-                        System.out.println(hunredUp(inputRay[1]));
-                    } else {
-                        System.out.print(hunredUp(inputRay[1]));
-                        System.out.print(" and "+tenUp(inputRay[2], tenDown(inputRay[3])) + "\n");
-                    }
+                case 3 :
+                    dochangtram="ba";
+                    break;
+                case 4 :
+                    dochangtram="bốn";
+                    break;
+                case 5 :
+                    dochangtram="năm";
+                    break;
+                case 6 :
+                    dochangtram="sáu";
+                    break;
+                case 7 :
+                    dochangtram="bảy";
+                    break;
+                case 8 :
+                    dochangtram="tám";
+                    break;
+                case 9 :
+                    dochangtram="chín";
+                    break;
+
+            }
+            switch (hangchuc){
+                case 1 :
+                    dochangchuc="một";
+                    break;
+                case 2:
+                    dochangchuc = "hai";
+                    break;
+                case 3 :
+                    dochangchuc="ba";
+                    break;
+                case 4 :
+                    dochangchuc="bốn";
+                    break;
+                case 5 :
+                    dochangchuc="năm";
+                    break;
+                case 6 :
+                    dochangchuc="sáu";
+                    break;
+                case 7 :
+                    dochangchuc="bảy";
+                    break;
+                case 8 :
+                    dochangchuc="tám";
+                    break;
+                case 9 :
+                    dochangchuc="chín";
+                    break;
+
+            }
+            switch (donvi){
+                case 1 :
+                    docdonvi="một";
+                    break;
+                case 2:
+                    docdonvi = "hai";
+                    break;
+                case 3 :
+                    docdonvi="ba";
+                    break;
+                case 4 :
+                    docdonvi="bốn";
+                    break;
+                case 5 :
+                    docdonvi="năm";
+                    break;
+                case 6 :
+                    docdonvi="sáu";
+                    break;
+                case 7 :
+                    docdonvi="bảy";
+                    break;
+                case 8 :
+                    docdonvi="tám";
+                    break;
+                case 9 :
+                    docdonvi="chín";
+                    break;
+
             }
 
-        }
 
-    }
-
-    private static String tenDown(String number) {
-        switch (number) {
-            case "0":
-                return "";
-            case "1":
-                return "one";
-            case "2":
-                return "two";
-            case "3":
-                return "three";
-            case "4":
-                return "four";
-            case "5":
-                return "five";
-            case "6":
-                return "six";
-            case "7":
-                return "seven";
-            case "8":
-                return "eight";
-            case "9":
-                return "nine";
-            default:
-                return "can't read this";
+            System.out.print(dochangtram +" "+ dochangchuc+" "+ docdonvi);
         }
     }
-
-    private static String tenUp(String number, String previous) {
-        switch (number) {
-            case "0":
-                return previous;
-            case "1":
-                switch (previous) {
-                    case "":
-                        return "ten";
-                    case "one":
-                        return "eleven";
-                    case "two":
-                        return "twelve";
-                    case "three":
-                        return "thirdteen";
-                    default:
-                        return previous + "teen";
-                }
-            case "2":
-                return "twenty " + previous;
-            case "3":
-                return "thirdty " + previous;
-            default:
-                return tenDown(number) + "ty" + " " + previous;
-
-        }
-    }
-
-    private static String hunredUp(String number) {
-        return tenDown(number) + " hunred";
-    }
-}
