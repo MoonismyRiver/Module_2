2bcaac8f7b65b63f07d7cd0387c0bfa414539f79
MoonismyRiver <keepermen1962gmail.com>
2022-06-02 15:43:28 +0700

    sua bai tap

--- src/Threading/BaiTapTimSoNguyenTo/LazyPrimeFactorization.java
+++ src/Threading/BaiTapTimSoNguyenTo/LazyPrimeFactorization.java
@@ -1,2 +1,28 @@
-package Threading.BaiTapTimSoNguyenTo;public class LazyPrimeFactorization {
+package Threading.BaiTapTimSoNguyenTo;
+
+public class LazyPrimeFactorization implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("---Thread 1: Tìm số nguyên tố  chưa tối ưu---");
+
+        for (int n = 2; n < 100; n++) {
+            boolean check = true;
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    check = false;
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+            if (check) {
+                System.out.println("thread 1 :" + n);
+            }
+
+        }
+        System.out.println("Thread 1 done!");
+
+    }
 }
--- src/Threading/BaiTapTimSoNguyenTo/OptimizedPrimeFactorization.java
+++ src/Threading/BaiTapTimSoNguyenTo/OptimizedPrimeFactorization.java
@@ -1,2 +1,26 @@
-package Threading.BaiTapTimSoNguyenTo;public class OptimizedPrimeFactorization {
+package Threading.BaiTapTimSoNguyenTo;
+
+public class OptimizedPrimeFactorization implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("---Thread 2 : Tìm số nguyên tố tối ưu---");
+
+        for (int n = 2; n < 100; n++) {
+            boolean check = true;
+            for (int i = 2; (i < Math.sqrt(n) || i == Math.sqrt(n)); i++) {
+                if (n % i == 0) {
+                    check = false;
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+            if (check) {
+                System.out.println("thread 2 :" + n);
+            }
+        }
+        System.out.println("Thread 2 done!");
+    }
 }
